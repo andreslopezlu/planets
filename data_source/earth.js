b106d6c8c10d7c7e2d66fdccea2783cbb562b4af
immprada <im.mprada@gmail.com>
2022-07-19 00:15:33 -0500

    Add earth

--- data_source/earth.js
+++ data_source/earth.js
@@ -1,9 +1,13 @@
+const basePath = '../assets/images/planets/earth'
+  
+const planetEarth = {  
   overviewContent: {
     data: "Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.",
     source: {
       label: "Wikipedia",
       url: "https://en.wikipedia.org/wiki/Earth",
     },
+    image: `${basePath}/general.svg`,
   },
   internalStructure: {
     data: "Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle.",
@@ -11,6 +15,7 @@
       label: "Wikipedia",
       url: "https://en.wikipedia.org/wiki/Earth#Internal_structure",
     },
+    image: `${basePath}/internal.svg`,
   },
   surfaceGeology: {
     data: "The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors.",
@@ -18,6 +23,7 @@
       label: "Wikipedia",
       url: "https://en.wikipedia.org/wiki/Earth#Surface",
     },
+    images: [`${basePath}/general.svg`, `${basePath}/geo.png`],
   },
   meaasures: {
     rotation: 0.99,
@@ -25,3 +31,4 @@
     radius: 6371,
     temperature: 16
   }
+}
