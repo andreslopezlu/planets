67a0348ec104f2f324a17c13791291c064d8c5a0
immprada <im.mprada@gmail.com>
2022-07-19 07:08:23 -0500

    Add mars

---
+++ data_source/mars.js
@@ -0,0 +1,34 @@
+const basePath = '../assets/images/planets/mars'  
+  
+const planetMars = {
+  overviewContent: {
+    data: `Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the "Red Planet".`,
+    source: {
+      label: `Wikipedia`,
+      url: `https://en.wikipedia.org/wiki/Mars`,
+    },
+    image: `${basePath}/general.svg`,
+  },
+  internalStructure: {
+    data: `Like Earth, Mars has differentiated into a dense metallic core overlaid by less dense materials. Scientists initially determined that the core is at least partially liquid. Current models of its interior imply a core consisting primarily of iron and nickel with about 16–17% sulfur.`,
+    source: {
+      label: `Wikipedia`,
+      url: `https://en.wikipedia.org/wiki/Mars#Internal_structure`,
+    },
+    image: `${basePath}/internal.svg`,
+  },
+  surfaceGeology: {
+    data: `Mars is a terrestrial planet whose surface consists of minerals containing silicon and oxygen, metals, and other elements that typically make up rock. The surface is primarily composed of tholeiitic basalt, although parts are more silica-rich than typical basalt.`,
+    source: {
+      label: `Wikipedia`,
+      url: `https://en.wikipedia.org/wiki/Mars#Surface_geology`,
+    },
+    images: [`${basePath}/general.svg`, `${basePath}/geo.png`],
+  },
+  meaasures: {
+    rotation: 1.03,
+    revolution: 1.88,
+    radius: 3389.5,
+    temperature: -28,
+  }
+}
