8b2f29ababe62d59982d04ff9dfe800e795ec363
immprada <im.mprada@gmail.com>
2022-07-19 15:05:21 -0500

    Add import script

---
+++ main.js
@@ -0,0 +1,13 @@
+import './data_source/earth';
+import './data_source/jupiter';
+import './data_source/mars';
+import './data_source/neptune';
+import './data_source/saturn';
+import './data_source/uranus';
+import './data_source/venus';
+
+console.log({
+  earth: planetEarth,
+  jupiter: planetJupiter,
+  mars: planetMars,
+})
