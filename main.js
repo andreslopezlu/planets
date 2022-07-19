8367da01d25d6faf5e20c3d2256fae67a4873f2d
immprada <im.mprada@gmail.com>
2022-07-19 16:00:48 -0500

    Merge planets data into planetsData object

--- main.js
+++ main.js
@@ -6,8 +6,35 @@ import './data_source/saturn';
 import './data_source/uranus';
 import './data_source/venus';
 
-console.log({
-  earth: planetEarth,
-  jupiter: planetJupiter,
-  mars: planetMars,
-})
+const planetsData ={
+  earth: {
+    title: `Earth`,
+    data: planetEarth
+  },
+  jupiter: {
+    title: `Jupiter`,
+    data: planetJupiter
+  },
+  mars: {
+    title: `Mars`,
+    data: planetMars
+  },
+  neptune: {
+    title: `Neptune`,
+    data: planetNeptune
+  },
+  saturn: {
+    title: `Saturn`,
+    data: planetSaturn
+  },
+  uranus: {
+    title: `Uranus`,
+    data: planetUranus
+  },
+  venus: {
+    title: `Venus`,
+    data: planetVenus
+  },
+}
+
+console.log(planetsData)
