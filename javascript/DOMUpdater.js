a69426b9ec17e60b56d5dd51f0aa1852782a9f5e
immprada <im.mprada@gmail.com>
2022-07-20 17:18:34 -0500

    Update Fields content

--- javascript/DOMUpdater.js
+++ javascript/DOMUpdater.js
@@ -15,20 +15,18 @@ const updateDOMPlanetInfo = (planet, section) => {
     temperature,
   } = getDOMObjects()
 
-  console.log(title)
-
-  // const title = document.getElementById('planet-name')
-  // const tag = document.getElementById('tag-earth')
-  // const principalImg = document.getElementById('planet-principal-image')
-  // const secondarImg = document.getElementById('planet-second-image')
-
-
-  // title.innerText = planetInfo.title
-  // principalImg.src = planetInfo.data.surfaceGeology.images[0]
-  // secondarImg.src = planetInfo.data.surfaceGeology.images[1]
-  // tag.addClass('active')
   updateTextContent(title, planetInfo.title)
   updateTextContent(info, planetInfo.data[section].data)
+  updateLinkContent(source, planetInfo.data[section].source.url, planetInfo.data[section].source.label)
+  updateTextContent(rotation, `${planetInfo.data.meaasures.rotation} DAYS`)
+  updateTextContent(revolution, `${planetInfo.data.meaasures.revolution} DAYS`)
+  updateTextContent(radius, `${planetInfo.data.meaasures.radius} KM`)
+  updateTextContent(temperature, `${planetInfo.data.meaasures.temperature}°C`)
 }
 
 const updateTextContent = (element, content) => (element.innerText = content)
+
+const updateLinkContent = (link, url, content) => {
+  link.text = content
+  link.href = url
+}
