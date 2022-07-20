ee532d00457b938f819dcc6db6133c76296c9092
immprada <im.mprada@gmail.com>
2022-07-20 16:09:05 -0500

    Add section to hash

--- javascript/DOMManager.js
+++ javascript/DOMManager.js
@@ -1,29 +1,42 @@
+const knownHash = {
+  planets: ['earth', 'jupiter', 'mars', 'neptune', 'saturn', 'uranus', 'venus'],
+  sections: ['overviewContent', 'internalStructure', 'surfaceGeology'],
+}
+
+// Listen to hash changes, and start DOM modifications
+
 window.addEventListener('hashchange', function() {
 	const newHash = new URL(document.URL).hash
-  const currentPlanetOptions = newHash.split('#')[1].split('-')
-  const currentPlanet = currentPlanetOptions[0]
-  const currentSection = currentPlanetOptions[1]
+  const currentPlanetOptions = newHash ? newHash.split('#')[1].split('-') : undefined
+  const currentPlanet = currentPlanetOptions ? currentPlanetOptions[0] : undefined
+  const currentSection = currentPlanetOptions ? currentPlanetOptions[1] : undefined
+
+  console.log(currentPlanetOptions)
+  console.log(currentPlanet)
+  console.log(currentSection)
 
   updateDOMPlanetInfo(currentPlanet, currentSection)
 });
 
 
-
-
 // Thia for onload at Body
-// overviewContent, internalStructure, surfaceGeology
+// It update hash to a default value, if initial is no hash at URL
 
 const checkPlanet = () => {
   const curentUrl = new URL(document.URL)
-  const currentHash = curentUrl.hash
-  const currentPlanet = currentHash.split('#')[1]
+  const newHash = curentUrl.hash
+  const currentPlanetOptions = newHash ? newHash.split('#')[1].split('-') : undefined
+  const currentPlanet = currentPlanetOptions ? currentPlanetOptions[0] : undefined
+  const currentSection = currentPlanetOptions ? currentPlanetOptions[1] : undefined
 
-  if(!currentPlanet) {
+  console.log(curentUrl)
+
+  if(!newHash) {
     const URLToGo = new URL(document.URL)
 
-    URLToGo.hash = '#earth'
+    URLToGo.hash = '#earth-overviewContent'
     return document.location.href = URLToGo
   }
-
-  updateDOMPlanetInfo(currentPlanet)
+  
+  updateDOMPlanetInfo(currentPlanet, currentSection)
 }
