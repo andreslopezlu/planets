a57d85ed6f9e67dfb92690586358fafbbc926e77
immprada <im.mprada@gmail.com>
2022-07-20 15:38:46 -0500

    Move js to javascript folder

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,10 @@
-import './data_source/earth';
-import './data_source/jupiter';
-import './data_source/mars';
-import './data_source/neptune';
-import './data_source/saturn';
-import './data_source/uranus';
-import './data_source/venus';
+import '../data_source/earth';
+import '../data_source/jupiter';
+import '../data_source/mars';
+import '../data_source/neptune';
+import '../data_source/saturn';
+import '../data_source/uranus';
+import '../data_source/venus';
 
 import './DOMObjects';
 import './DOMUpdater';
